73f5a1ca70568a3bfce3064b6d732d105c8b49a4
Deepak Ramani <dr563105@gmail.com>
2025-03-28 19:12:01 +0530

    removed whitespaces and renamed erp filenames

--- docker/postgres_init_db/03_bronze_load_data.sql
+++ docker/postgres_init_db/03_bronze_load_data.sql
@@ -8,16 +8,13 @@ COPY bronze.crm_cust_info
 FROM '/home/input_data/source_crm/cust_info.csv'
 WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
-
 COPY bronze.crm_prd_info
 FROM '/home/input_data/source_crm/prd_info.csv'
 WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
-
 COPY bronze.crm_sales_details
 FROM '/home/input_data/source_crm/sales_details.csv'
 WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
-
 
 COPY bronze.erp_loc_a101
 FROM '/home/input_data/source_erp/loc_a101.csv'
@@ -26,7 +23,6 @@ WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 COPY bronze.erp_cust_az12
 FROM '/home/input_data/source_erp/cust_az12.csv'
 WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
-
 
 COPY bronze.erp_px_cat_g1v2
 FROM '/home/input_data/source_erp/px_cat_g1v2.csv'
