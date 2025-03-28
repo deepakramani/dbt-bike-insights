c9161c7b34195a248ffdc885458e80638964092c
Deepak Ramani <dr563105@gmail.com>
2025-03-28 19:16:23 +0530

    added script to install duckdb

---
+++ docker/scripts/install_duckdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~
+if [[ ! -e soft ]]; then
+    mkdir -p soft
+fi
+cd ~/soft
+sudo apt update && sudo apt install unzip
+wget -q https://github.com/duckdb/duckdb/releases/download/v1.2.1/duckdb_cli-linux-amd64.zip -O duckdb_cli.zip
+unzip duckdb_cli.zip
+chmod +x duckdb
+echo 'export PATH="/home/'$USER'/soft:$PATH"' >> ~/.bashrc
+rm duckdb_cli.zip
+cd ~
