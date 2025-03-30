2f69e238cfdfb79cfc3daa4e1bebaa9259ef889d
Deepak Ramani <dr563105@gmail.com>
2025-03-31 00:07:49 +0530

    added new logic and column for SCD2

--- dbt_transform/models/silver/crm_prd_info.sql
+++ dbt_transform/models/silver/crm_prd_info.sql
@@ -1,33 +1,62 @@
-WITH source AS (
+WITH source_table AS (
     SELECT 
         *
     FROM {{ source('silver_source','crm_prd_info')}}
 ),
-cleaned_prd_info AS (
+ranked_data AS (
     SELECT
         prd_id,
-        REPLACE(SUBSTR(prd_key, 1, 5), '-', '_') AS cat_id,
-        SUBSTR(prd_key, 7, LENGTH(prd_key)) AS prd_key,
+        prd_key,
         prd_nm,
-        COALESCE(prd_cost, 0) AS prd_cost,
+        prd_cost,
+        prd_line,
         CASE
-            WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountains'
-            WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-            WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Sales'
-            WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-            ELSE 'n/a'
-        END AS prd_line,
-        CAST(prd_start_dt AS DATE) AS prd_start_date
-    FROM source
+            WHEN prd_end_dt IS NOT NULL AND prd_end_dt < prd_start_dt THEN prd_end_dt
+        ELSE prd_start_dt
+        END AS prd_start_date,
+        CASE
+            WHEN prd_end_dt IS NOT NULL AND prd_end_dt < prd_start_dt THEN prd_start_dt
+            WHEN prd_end_dt IS NULL THEN '2099-12-31' -- Far Future Date
+            ELSE prd_end_dt
+        END AS prd_end_date,
+        ROW_NUMBER() OVER (PARTITION BY prd_key ORDER BY prd_start_dt) AS rn
+    FROM source_table
 ),
-final_prd_info AS (
+adjusted_dates as(
     SELECT
-        *,
-        LEAD(prd_start_date, 1) OVER (
-            PARTITION BY prd_key
-            ORDER BY prd_start_date
-        ) - 1 AS prd_end_dt,
-        now() as dwh_create_date
-    FROM cleaned_prd_info
+        rd.*,
+        LAG(prd_end_date, 1) OVER (PARTITION BY prd_key ORDER BY prd_start_date) AS prev_end_dt
+    FROM ranked_data rd
+),
+final_dates as (SELECT
+    ad.prd_id,
+    replace(substr(prd_key, 1,5),'-','_') as cat_id,
+    substr(prd_key, 7, length(prd_key)) as prd_key,
+    prd_nm,
+    coalesce(prd_cost,0) as prd_cost,
+    case
+       when upper(trim(prd_line)) = 'M' then 'Mountains'
+       when upper(trim(prd_line)) = 'R' then 'Road'
+       when upper(trim(prd_line)) = 'S' then 'Sales'
+       when upper(trim(prd_line)) = 'T' then 'Touring'
+       else 'n/a'
+    end as prd_line,
+    CASE
+        WHEN ad.prev_end_dt IS NOT NULL AND ad.prev_end_dt > ad.prd_start_date THEN ad.prev_end_dt -- - INTERVAL '1 day'
+        ELSE ad.prd_start_date
+    END AS prd_start_date,
+    CASE
+        WHEN ad.prev_end_dt IS NULL AND ad.prd_end_date <> '2099-12-31'::DATE
+             THEN ad.prd_end_date - INTERVAL '1 minute'
+        ELSE ad.prd_end_date
+    END AS prd_end_date
+    FROM adjusted_dates ad
+    order by prd_id
 )
-SELECT * FROM final_prd_info
+select
+    *,
+    case
+        when prd_end_date = '2099-12-31' then prd_start_date + interval '1' day
+        else prd_end_date + interval '1' day
+    end as updated_at
+from final_dates
