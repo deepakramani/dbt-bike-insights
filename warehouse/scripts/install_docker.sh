a264db520e0ceaa80e363abe811a7e83fd55c9c9
Deepak Ramani <dr563105@gmail.com>
2025-04-26 16:59:09 +0530

    corrected typos

--- warehouse/scripts/install_docker.sh
+++ warehouse/scripts/install_docker.sh
@@ -8,4 +8,4 @@ chmod +x docker-compose
 echo 'export PATH="/home/'$USER'/soft:$PATH"' >> ~/.bashrc
 sudo groupadd docker
 sudo usermod -aG docker $USER
-cd ~/etl_with_dbt_dwh
+cd ~/dbt-bike-insights
