e55787f1983709793f5b442269320feb75aeb19b
Deepak Ramani <dr563105@gmail.com>
2025-04-27 01:02:56 +0530

    changed source to dot for shell script execution

--- terraform/main.tf
+++ terraform/main.tf
@@ -178,7 +178,7 @@ resource "null_resource" "setup_vm" {
       "git checkout feature-terraform",
       "make install_docker",
       "make install_conda",
-      "source $HOME/soft/miniforge3/etc/profile.d/conda.sh",
+      ". $HOME/soft/miniforge3/etc/profile.d/conda.sh",
       "$HOME/soft/miniforge3/bin/conda activate base",
       "make install_dbt",
       "make install_duckdb"
