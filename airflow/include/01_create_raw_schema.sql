72f17ae4147011fa995684b722f51e553262658a
Deepak Ramani <dr563105@gmail.com>
2025-07-21 20:49:35 +0530

    added monitoring schema

--- airflow/include/01_create_raw_schema.sql
+++ airflow/include/01_create_raw_schema.sql
@@ -6,5 +6,7 @@ Create 'raw' schema to ingest raw data.
 
 CREATE SCHEMA IF NOT EXISTS raw;
 
+CREATE SCHEMA IF NOT EXISTS monitoring;
+
 CREATE EXTENSION IF NOT EXISTS unaccent;
 COMMENT ON EXTENSION unaccent IS 'Text normalization extension to remove accents';
